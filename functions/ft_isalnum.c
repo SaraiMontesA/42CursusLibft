7c3e4a2ccef42789d67ae61370225ab189e59996
SaraiMontesA <SaraimontesA@gmail.com>
2023-11-28 17:37:31 +0100

    Subo archivo ft_isalnum.c

---
+++ functions/ft_isalnum.c
@@ -0,0 +1,34 @@
+/******************************************************************************/
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_isalnum.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sarmonte <sarmonte@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/21 20:24:43 by sarmonte          #+#    #+#             */
+/*   Updated: 2023/11/28 15:58:06 by sarmonte         ###   ########.fr       */
+/*                                                                            */
+/******************************************************************************/
+
+// Hago un include del libft.h para que me incluya las librerias necesarias
+#include "../libft.h"
+
+int	ft_isalnum(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')
+		|| (c >= '0' && c <= '9'))
+		return (1);
+	else
+		return (0);
+}
+
+/*
+int	ft_isalpha(int c)
+{
+	// si c está entre a y z, o si c está entre A y Z
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1); // Devuelve 1
+	else
+		return (0); // Devuelve 0
+}
+*/
